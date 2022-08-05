ad8a7d80b3dd01f422d6df995732dc9bdccfba19
mabu104 <thanhhoi104233@gmail.com>
2022-08-05 21:26:22 +0700

    Yaly Web commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState, useContext, createContext } from "react";
+import { View, StyleSheet, SafeAreaView } from "react-native";
+import Login from "./pages/Login";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { Blog } from "./pages/Blog";
+import SideBar from './components/SideBar';
+import NavBar from "./components/NavBar";
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import "./App.css";
+import { UserContext } from "./contexts/UserContext";
+//import { Logged } from './pages/UserDetails';
+//export const UserContext = createContext();
+function App() {
+  const [state, dispatch] = useState({
+    userName: '',
+    password: '',
+    logged: false,
+    user: { user: '', no: '', recSeller: 0, status: 0 },
+    site: { code: '', id: 0, name: '' }
+  });
+  return (
+
+    <Router>
+      <SideBar>
+        {/* <NavBar /> */}
+        {/* <div className="pages"> */}
+        <UserContext.Provider value={{ state, dispatch }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </UserContext.Provider>
+        {/* </div> */}
+      </SideBar>
+    </Router >
+
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+export default App;
+
+
---
+++ src/components/SideBar.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import {
+    FaTh,
+    FaBars,
+    FaUserAlt,
+    FaRegChartBar,
+    FaCommentAlt,
+    FaShoppingBag,
+    FaTshirt,
+    FaThList,
+    FaRegUser
+}from "react-icons/fa";
+import {
+    AiOutlineHome,
+    AiOutlineAppstore,
+    AiOutlineSkin,
+    AiOutlinePieChart,
+}from "react-icons/ai";
+import { NavLink } from 'react-router-dom';
+import "./SideBar.css";
+
+const SideBar= ({children})=> {
+    const[isOpen ,setIsOpen] = useState(false);
+    const toggle = () => setIsOpen (!isOpen);
+    const menuItem=[
+        {
+            path:"/",
+            name:"Trang chủ",
+            icon:<AiOutlineHome/>
+        },
+        {
+            path:"/about",
+            name:"Sản phẩm",
+            icon:<AiOutlineAppstore/>
+        },
+        {
+            path:"/contact",
+            name:"Catalogue",
+            icon:<AiOutlineSkin/>
+        },
+        {
+            path:"/blog",
+            name:"Báo cáo",
+            icon:<AiOutlinePieChart/>
+        },
+
+        {
+            path:"/login",
+            name:"Tài khoản",
+            icon:<FaRegUser/>
+        },
+
+    ]
+    return (
+        <div className="sidebar_container">
+           <div  className="sidebar">
+               {/* <div className="top_section">
+                   <h1 style={{display: isOpen ? "block" : "none"}} className="logo">Logo</h1>
+                   <div style={{marginLeft: isOpen ? "50px" : "0px"}} className="bars">
+                       <FaBars onClick={toggle}/>
+                   </div>
+               </div> */}
+               {
+                   menuItem.map((item, index)=>(
+                       <NavLink to={item.path} key={index} className="link" activeclassName="active">
+                           <div className="icon">{item.icon}</div>
+                           <div  className="link_text">{item.name}</div>
+                       </NavLink>
+                   ))
+               }
+           </div>
+           <main>{children}</main>
+        </div>
+    );
+};
+
+export default SideBar;
