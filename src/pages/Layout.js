60a645bdecf1ed579ec2a1b5209bc6b89b30e9c1
mabu104 <thanhhoi104233@gmail.com>
2022-08-18 21:28:54 +0700

    Yaly Web commit

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,94 +1,36 @@
-import React, { useContext, useState, useMemo } from "react";
-import { Route, NavLink, BrowserRouter as Router, Routes, Navigate, Link, Outlet } from 'react-router-dom'
+import React, { useContext, useState, useMemo,useLayoutEffect,useEffect  } from "react";
+import { Navigate, Outlet } from 'react-router-dom'
 import SideBar from '../components/SideBar';
+import Backdrop from '../components/Backdrop';
+import ResponsiveNav from '../components/ResponsiveNav';
 import { UserContext } from '../contexts/UserContext';
-import {
-  FaTh,
-  FaBars,
-  FaUserAlt,
-  FaRegChartBar,
-  FaCommentAlt,
-  FaShoppingBag,
-  FaTshirt,
-  FaThList,
-  FaRegUser
-} from "react-icons/fa";
-import {
-  AiOutlineHome,
-  AiOutlineAppstore,
-  AiOutlineSkin,
-  AiOutlinePieChart,
-  AiOutlineClose
-} from "react-icons/ai";
+import { AiOutlineMenu } from "react-icons/ai";
+
 
 import "./Layout.css"
 
 export const Layout = () => {
   const { state, dispatch } = useContext(UserContext);
   const [logged, setLogged] = useState(state.logged)
-  const [sidebar, setSidebar] = useState(false);
-  const showSidebar = () => setSidebar(!sidebar);
-  const menuItem = [
-    {
-      path: "/",
-      name: "Trang chủ",
-      icon: <AiOutlineHome />
-    },
-    {
-      path: "/order",
-      name: "Đơn hàng",
-      icon: <AiOutlineAppstore />
-    },
-    {
-      path: "/catalogue",
-      name: "Catalogue",
-      icon: <AiOutlineSkin />
-    },
-    {
-      path: "/dashboard",
-      name: "Báo cáo",
-      icon: <AiOutlinePieChart />
-    },
-
-    {
-      path: "/user",
-      name: "Tài khoản",
-      icon: <FaRegUser />
-    },
-
-  ]
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [miniSidebar, setMiniSidebar] = useState(false);
+  const clickMenu = () => {
+    if(window.innerWidth<700) setShowSidebar(!showSidebar);
+    else setMiniSidebar(!miniSidebar);
+  }
+  //if (!logged) return <Navigate to="/login" replace={true} />;
   return (
-    (!logged) ? <Navigate to="/login" replace={true} /> :
-      <div className="layout-container">
-        <SideBar />
-        <div className="body-container">
-          <div className="appBar">
-            <FaBars className="icon-menu" onClick={showSidebar} ></FaBars>
-            <h1 className="name-bar">Yaly Couture</h1>
-            <div />
-          </div>
-
-          <Outlet />
-          {(sidebar) ?
-            <div className="sidebar-container-show">
-
-              <div className="sidebar-show">
-                <div className="appbar-show">
-                  <AiOutlineClose className="close-icon" onClick={showSidebar}></AiOutlineClose>
-                </div>
-                {
-                  menuItem.map((item, index) => (
-                    <NavLink to={item.path} key={index} className="link2" onClick={showSidebar}>
-                      <div className="icon2">{item.icon}</div>
-                      <div className="link_text2">{item.name}</div>
-                    </NavLink>
-                  ))
-                }
-              </div>
-            </div>
-
-            : <div></div>}
-        </div>
+    <div className="layout-container">
+      <SideBar mini={miniSidebar}/>
+      <div className="body-container">
+        <a className="appBar">
+          <AiOutlineMenu className="icon-menu" onClick={clickMenu}  ></AiOutlineMenu>
+        </a>  
+        <Backdrop show={showSidebar} closeResbar={setShowSidebar}/>    
+        <Outlet />
+        
+        <ResponsiveNav show={showSidebar} setShow={setShowSidebar} />
       </div>
+    </div>
   );
 };
